11ccb130f5f36168473405eec8a8f7c7532c39fa
Artūras <arturas.milius@koralis.lt>
2017-05-27 13:06:07 +0300

    Esports navigation and routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+
+const appRoutes: Routes = [
+  // { path: 'crisis-center', component: CrisisListComponent },
+  // { path: 'hero/:id',      component: HeroDetailComponent }
+  {path: '', component: HomeComponent},
+  { path: '404', component: PageNotFoundComponent },
+  {path: '**', redirectTo: '404'}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PageNotFoundComponent,
   ],
   imports: [
+    RouterModule.forRoot(appRoutes),
     BrowserModule,
     FormsModule,
     HttpModule
